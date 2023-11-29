353f19562520567c2c891ff21e367883f37f99cf
Elvis Djoumessi <e.djoumessi@verder-scientific.com>
2023-11-29 11:35:00 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<IStorageService, SessionStorageService>();
+builder.Services.AddScoped<AuthenticationService>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<AuthenticationService>());
+builder.Services.AddScoped<IAdminService, AdminService>();
+builder.Services.AddHttpClient<AuthenticationHttpClient>(client => client.BaseAddress = new Uri("https://localhost:5001/api/"));
+builder.Services.AddHttpClient<MembershipHttpClient>(client => client.BaseAddress = new Uri("https://localhost:6001/api/"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Using.cs
@@ -0,0 +1,15 @@
+﻿global using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using VOD.Admin.UI.Classes;
+global using VOD.Common.HttpClients;
+global using VOD.Common.Services;
+global using VOD.Common.DTOs;
+global using VOD.Common.Extensions;
+global using VOD.Admin.UI.Authentication;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using System.Net;
+global using System.Text;
+global using VOD.Common.Models;
+
+
